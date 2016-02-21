8d7c59d8243c1a3b5496ac2f5b10da6a3c652a08
Martin Georgiev <martin.georgiev@gmail.com>
2016-02-21 03:38:59 +0200

    Add Date class and use DateDiff for MyDate

---
+++ src/Date.php
@@ -0,0 +1,152 @@
+<?php
+
+class Date
+{
+    /**
+     * @var string
+     */
+    private $date;
+
+    /**
+     * @var string
+     */
+    private $dateFormat;
+
+    /**
+     * @var int
+     */
+    private $year;
+
+    /**
+     * @var int
+     */
+    private $month;
+
+    /**
+     * @var int
+     */
+    private $day;
+
+    /**
+     * @param string $date
+     * @param string $dateFormat
+     */
+    public function __construct($date, $dateFormat = 'YYYY/MM/DD')
+    {
+        $this->date = $date;
+        $this->dateFormat = $dateFormat;
+
+        $this->breakdownDateIntoYearMonthAndDay();
+    }
+
+    private function breakdownDateIntoYearMonthAndDay()
+    {
+        $datePartials = (array)$this->date;
+        $dateFormatMap = (array)$this->dateFormat;
+
+        $year = $month = $day = '';
+        foreach ($dateFormatMap as $position => $datePartial) {
+            switch ($datePartial) {
+                case 'Y':
+                    $year .= $datePartials[$position];
+                    break;
+                case 'M':
+                    $month .= $datePartials[$position];
+                    break;
+                case 'D':
+                    $day .= $datePartials[$position];
+                    break;
+            }
+        }
+
+        $this->year = $year !== '' ? (int)$year : null;
+        $this->month = $month !== '' ? (int)$month : null;
+        $this->day = $day !== '' ? (int)$day : null;
+
+        $this->throwExceptionIfNotValidDate();
+    }
+
+    private function throwExceptionIfNotValidDate()
+    {
+        $this->throwExceptionIfNotValidYear();
+        $this->throwExceptionIfNotValidMonth();
+        $this->throwExceptionIfNotValidDay();
+    }
+
+    private function throwExceptionIfNotValidYear()
+    {
+        if (is_null($this->year)) {
+            throw new Exception('Year cannot be extracted');
+        }
+    }
+
+    private function throwExceptionIfNotValidMonth()
+    {
+        if (is_null($this->month)) {
+            throw new Exception('Month cannot be extracted');
+        }
+        if ($this->month > 12 || $this->month < 1) {
+            throw new Exception('Invalid month');
+        }
+    }
+
+    private function throwExceptionIfNotValidDay()
+    {
+        if (is_null($this->day)) {
+            throw new Exception('Day cannot be extracted');
+        }
+
+        $monthsWith30Days = array(4, 6, 9, 11);
+        $monthsWith31Days = array(1, 3, 5, 7, 8, 10, 12);
+
+        if (
+            $this->day < 1 ||
+            ($this->month === 2 && !$this->isLeapYear() && $this->day > 28) ||
+            ($this->month === 2 && $this->isLeapYear() && $this->day > 29) ||
+            (in_array($this->month, $monthsWith30Days) && $this->day > 30) ||
+            (in_array($this->month, $monthsWith31Days) && $this->day > 31)
+        ) {
+            throw new Exception('Invalid day');
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMonth()
+    {
+        return $this->month;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDay()
+    {
+        return $this->day;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isLeapYear()
+    {
+        if ($this->year % 4 !== 0) {
+            return false;
+        } elseif ($this->year % 100 !== 0) {
+            return true;
+        } elseif ($this->year % 400 === 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
--- src/MyDate.php
+++ src/MyDate.php
@@ -1,18 +1,22 @@
 <?php
 
-  class MyDate {
+class MyDate
+{
+    /**
+     * @param string $start
+     * @param string $end
+     * @return stdClass
+     */
+    public static function diff($start, $end)
+    {
+        $dateDiff = new DateDiff($start, $end);
 
-    public static function diff($start, $end) {
-
-      // Sample object:
-      return (object)array(
-        'years' => null,
-        'months' => null,
-        'days' => null,
-        'total_days' => null,
-        'invert' => null
-      );
-
+        return (object)array(
+            'years' => $dateDiff->getYears(),
+            'months' => $dateDiff->getMonths(),
+            'days' => $dateDiff->getDays(),
+            'total_days' => $dateDiff->getTotalDays(),
+            'invert' => $dateDiff->getInvert(),
+        );
     }
-
-  }
+}
